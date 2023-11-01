b4d63f94aa1e1afadf0cee4f4acbffaea68603bc
Matthew D <42253757+CrimsonSquadron@users.noreply.github.com>
2023-11-01 16:04:58 -0500

    Added more tasks.

--- prod.ps1
+++ prod.ps1
@@ -1,19 +1,71 @@
-# Task 1: Disable anonymous enumeration of SAM accounts and shares
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "RestrictNullSessAccess" -Value 1
-
-# Task 2: Disable the Guest account
+# Task 1: Disable the Guest account
 Disable-LocalUser -Name "Guest"
 
-# Task 3: Ensure a secure account lockout duration exists
+# Task 2: Ensure a secure account lockout duration exists
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "LockoutDuration" -Value 30
 
-# Task 4: Limit local use of blank passwords to console only
+# Task 3: Limit local use of blank passwords to console only
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LimitBlankPasswordUse" -Value 1
 
-# Task 5: Enable the Windows Update Service
+# Task 4: Enable the Windows Update Service
 Set-Service -Name "wuauserv" -StartupType "Automatic"
 Start-Service -Name "wuauserv"
 
-# Task 6: Disable the Microsoft FTP Service
+# Task 5: Disable the Microsoft FTP Service
 Set-Service -Name "ftpsvc" -Status "Stopped"
 Set-Service -Name "ftpsvc" -StartupType "Disabled"
+
+# Task 6: Remove unauthorized users from the machine
+Remove-LocalUser -Name "vattu"
+Remove-LocalUser -Name "iroh"
+
+# Task 7: Add User tonraq 
+New-LocalUser -Name "tonraq" -Password "H@slo314@@@"
+
+# Task 8 : Remove unauthorized Administrators
+# Remove-LocalGroupMember -Group administrators -Member $iroh
+
+# Task 9: Password Age, etc
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "MaxPasswordAge" -Value 90
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "MinPasswordAge" -Value 10
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "MinimumPasswordLength" -Value 4
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PasswordComplexity" -Value 1
+
+#Task 10: Registry Changes
+powershell.exe Disable-WindowsOptionalFeature -Online -FeatureName smb1protocol -norestart
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\mrxsmb10" /v Start /t REG_DWORD /d 4 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /v SMB1 /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters" /v RestrictNullSessAccess /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v RestrictAnonymousSAM /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v RestrictAnonymous /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v EveryoneIncludesAnonymous /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v RestrictRemoteSAM /t REG_SZ /d "O:BAG:BAD:(A;;RC;;;BA)" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v UseMachineId /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LimitBlankPasswordUse /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\LSA\MSV1_0" /v allownullsessionfallback /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters" /v EnableSecuritySignature /t REG_DWORD /d 1 /f
+
+# Task 11: Audit Policy (AuditPol)
+Auditpol /set /subcategory:"Security Group Management" /success:enable /failure:enable
+Auditpol /set /subcategory:"Process Creation" /success:enable /failure:enable
+Auditpol /set /subcategory:"Logoff" /success:enable /failure:disable
+Auditpol /set /subcategory:"Logon" /success:enable /failure:enable 
+Auditpol /set /subcategory:"Filtering Platform Connection" /success:enable /failure:disable
+Auditpol /set /subcategory:"Removable Storage" /success:enable /failure:enable
+Auditpol /set /subcategory:"SAM" /success:disable /failure:disable
+Auditpol /set /subcategory:"Filtering Platform Policy Change" /success:disable /failure:disable
+Auditpol /set /subcategory:"IPsec Driver" /success:enable /failure:enable
+Auditpol /set /subcategory:"Security State Change" /success:enable /failure:enable
+Auditpol /set /subcategory:"Security System Extension" /success:enable /failure:enable
+Auditpol /set /subcategory:"System Integrity" /success:enable /failure:enable
+auditpol /set /category:"System","Account Management","Account Logon","Logon/Logoff","Policy Change" /failure:enable /success:enable  
+auditpol /set /category:"DS Access","Object Access" /failure:enable
+
+# Task 12: Remove any junk apps
+Get-Package -Provider Programs -IncludeWindowsInstaller -Name “Wireshark”
+Uninstall-Package -Name “Wireshark”
+
+# Task 20: Updates!
+# Install-Module PSWindowsUpdate
+# Add-WUServiceManager -MicrosoftUpdate
+#   Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot | Out-File "C:\($env.computername-Get-Date -f yyyy-MM-dd)-MSUpdates.log" -Force
